3deee777196009f3a0b8635ad2aab98d4987a26f
scensorECHO <scensorECHO@users.noreply.github.com>
2015-08-29 10:17:59 -0400

    Java file posted

---
+++ Tester.java
@@ -0,0 +1,11 @@
+public class Tester 
+{
+  public static void main(String[] args)
+  {
+    int i = 0;
+    int x = 5;
+    String name = "Tom";
+    
+    System.out.println("My name is "+name);
+  }
+}
